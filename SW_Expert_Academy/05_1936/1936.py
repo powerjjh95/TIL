0a22e257127577930fe26707e657535ded3bc3ec
JHJang95 <powerjjh95@gmail.com>
2022-08-09 00:52:25 +0900

    20220808

---
+++ SW_Expert_Academy/05_1936/1936.py
@@ -0,0 +1,10 @@
+A, B = list(map(int, input().split()))
+# scissor = 1
+# rock = 2
+# paper = 3
+# scissor(1)-rock(2): rock(2)
+# rock(2)-paper(3): paper(3)
+# paper(3)-scissor(1): scissor(1)
+
+if (A + B) %3 == :
+    print(A)
