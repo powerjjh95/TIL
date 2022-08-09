c2b6364f76223728c0734b5761b79e7799044559
장준호 <powerjjh95@gmail.com>
2022-08-09 17:45:03 +0900

    20220729

--- SW_Expert_Academy/05_1936/1936.py
+++ SW_Expert_Academy/05_1936/1936.py
@@ -6,6 +6,17 @@ A, B = list(map(int, input().split()))
 # rock(2)-paper(3): paper(3)
 # paper(3)-scissor(1): scissor(1)
 
-if A == 1 and B == 2:
-    print(A)
-elif A ==
+# A가 이기는 경우
+if A == 2 and B == 1:
+    print('A')
+elif A == 3 and B == 2:
+    print('A')
+elif A == 1 and B == 3:
+    print('A')
+# B가 이기는 경우
+if B == 2 and A == 1:
+    print('B')
+elif B == 3 and A == 2:
+    print('B')
+elif B == 1 and A == 3:
+    print('B')
