14e5aa11e034cbfa6af1c88489d49573d069e485
powerjjh95 <powerjjh95@gmail.com>
2023-03-13 11:34:12 +0900

    'auto-commiter'

---
+++ Algorithm/javascript/BAEKJOON/1001.js
@@ -0,0 +1,20 @@
+function solution(input) {
+  const [a, b] = input;
+  const answer = a - b;
+  console.log(answer);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  input = line;
+  rl.close();
+}).on("close", function () {
+  list = input.split(" ").map((el) => parseInt(el));
+  solution(list);
+  process.exit();
+});
