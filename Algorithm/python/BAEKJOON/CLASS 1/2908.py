7db79a545f4c44316edc2db4c3a93ec65967bf0c
powerjjh95 <powerjjh95@gmail.com>
2023-04-05 09:14:25 +0900

    'auto-commiter'

--- Algorithm/python/BAEKJOON/CLASS 1/2908.py
+++ Algorithm/python/BAEKJOON/CLASS 1/2908.py
@@ -1,1 +1,10 @@
-number1, number2
+number1, number2 = map(int, input().split())
+number1_list = list(map(str, str(number1)))
+number2_list = list(map(str, str(number2)))
+number1 = int("".join(number1_list[::-1]))
+number2 = int("".join(number2_list[::-1]))
+if number1 > number2:
+    answer = number1
+else:
+    answer = number2
+print(answer)
