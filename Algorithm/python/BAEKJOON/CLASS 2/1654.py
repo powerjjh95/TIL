28680ed20ffed653f3320b3e9ca023a8779f2ff6
powerjjh95 <powerjjh95@gmail.com>
2023-04-29 14:02:00 +0900

    'auto-commiter'

---
+++ Algorithm/python/BAEKJOON/CLASS 2/1654.py
@@ -0,0 +1,9 @@
+K, N = map(int, input().split())
+length_list = []
+for _ in range(K):
+    K_length = int(input())
+    length_list += [K_length]
+    N_length =
+    for i in range(len(length_list)):
+        while True:
+            if length_list[i]
