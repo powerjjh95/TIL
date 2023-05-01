e5260cce48bfa92c09df4d1646f6acb3bada47ee
powerjjh95 <powerjjh95@gmail.com>
2023-05-01 17:52:23 +0900

    'auto-commiter'

--- Algorithm/python/BAEKJOON/CLASS 2/1654.py
+++ Algorithm/python/BAEKJOON/CLASS 2/1654.py
@@ -1,9 +1,18 @@
 K, N = map(int, input().split())
 length_list = []
+N_length = 0
+count = 0
 for _ in range(K):
     K_length = int(input())
     length_list += [K_length]
-    N_length =
+N_length_first = sum(length_list) // N // 100
+N_length = N_length_first * 100
+while True:
     for i in range(len(length_list)):
-        while True:
-            if length_list[i]
+        length_list_i = length_list[i]
+        if length_list_i > N_length:
+            length_list_i = length_list_i - N_length
+            count += 1
+            # print(length_list_i)
+        else:
+            break
