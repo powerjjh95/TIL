13c31975cac34769f70bfc50403aa4ce28dfe4f2
powerjjh95 <powerjjh95@gmail.com>
2023-05-31 09:38:58 +0900

    'auto-commiter'

--- Algorithm/python/BAEKJOON/CLASS 2/1874.py
+++ Algorithm/python/BAEKJOON/CLASS 2/1874.py
@@ -1,7 +1,4 @@
-# TRY1
-# 1부터 n까지 오름차순 으로 나열되어 있는 Stack 생성(sequence)
-# 입력 받은 수열(stack_list)
-# sequence와 stack_list가 동일해 지는 경우까지 while문 실행
+# 1부터 하나씩 push를 받으며
 
 n = int(input())
 stack = []
@@ -13,12 +10,18 @@ for _ in range(n):
     number = int(input())
     while current <= number: # 입력한 수를 만날 때까지 오름차순으로 push
         stack.append(current)
-print(stack)
+        calculation.append("+")
+        current += 1
+        # 입력한 수를 만나면 while문 탈출. 즉 current = number일 때 까지 while문을 돌아 stack을 쌓는다.
 
+    if stack[-1] == number: # stack(list)의 마지막
+        stack.pop()
+        calculation.append("-")
+    else:
+        print("NO")
+        flag = 1
+        break
 
-
-
-
-
-
-
+if flag == 0:
+    for i in calculation:
+        print(i)
