951fc48a408fc9598b58ebc2b2268faf968d9292
powerjjh95 <powerjjh95@gmail.com>
2023-07-06 11:35:14 +0900

    20230706

---
+++ Algorithm/javascript/BAEKJOON/CLASS 1/27866.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on("line", (line) => {
+  if (!line) {
+    rl.close();
+  } else {
+    input.push(line);
+  }
+}).on("close", () => {
+  solution(input);
+  process.exit();
+});
+
+function solution(input) {
+  let S = input[0];
+  let i = input[1] - 1;
+  console.log(S[i]);
+}
