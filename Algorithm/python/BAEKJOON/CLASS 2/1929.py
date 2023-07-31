3ea0cf0cd6451f206ad49fb80079810c125b448c
powerjjh95 <powerjjh95@gmail.com>
2023-07-31 16:21:17 +0900

    20230731

--- Algorithm/python/BAEKJOON/CLASS 2/1929.py
+++ Algorithm/python/BAEKJOON/CLASS 2/1929.py
@@ -6,20 +6,35 @@
         * 소수는 1개의 짝수(2)와 홀수로 구성되어 있다.
 '''
 
-# TRY 2
+'''
+* Q. # TRY 2 / 위의 Logic이면 2에서 걸려야 할 것 같은데 안걸리는 것이 이해가 되지 않는다.
+    * A. # TRY 3 / number ** 0.5는 제곱근을 구하는 것이므로, 2의 경우 divisor가 range(2, 2)로 출력이 되지 않는다. 그래서 해결이 된다.
+'''
+
+# TRY 3
 M, N = map(int, input().split(" "))
 
 for number in range(M, N+1):
     if number == 1:
         continue
-    for divisor in range(2, int(number/2) + 1): # 다음 줄의 divisor의 범위는 number를 반으로 나누어 나누어 지는 수가 있는지 확인
+    for divisor in range(2, int(number ** 0.5) + 1): # TRY 2에서는 number/2라고 착각을 하여 오류 발생
         if number % divisor == 0: # 'number가 합성수'라는 의미
             break
     else:
         print(number)
-'''
-* Q. 위의 Logic이면 2에서 걸려야 할 것 같은데 안걸리는 것이 이해가 되지 않는다.
-'''
+
+# # TRY 2
+# M, N = map(int, input().split(" "))
+#
+# for number in range(M, N+1):
+#     if number == 1:
+#         continue
+#     for divisor in range(2, int(number/2) + 1): # 다음 줄의 divisor의 범위는 number를 반으로 나누어 나누어 지는 수가 있는지 확인
+#         if number % divisor == 0: # 'number가 합성수'라는 의미
+#             break
+#     else:
+#         print(number)
+
 
 # # TRY 1
 # M, N = map(int, input().split(" "))
