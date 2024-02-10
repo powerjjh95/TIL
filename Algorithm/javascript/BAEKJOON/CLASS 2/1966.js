3a7d7a158f1b76eeb8b752ce7f82b1cc36e77992
powerjjh95 <powerjjh95@gmail.com>
2024-02-10 13:10:22 +0900

    20240210

--- Algorithm/javascript/BAEKJOON/CLASS 2/1966.js
+++ Algorithm/javascript/BAEKJOON/CLASS 2/1966.js
@@ -17,12 +17,36 @@ rl.on("line", (line) => {
 });
 
 function solution(input) {
-  console.log(input);
+  // console.log(input);
   let testCase = input[0];
   for (let i = 1; i < input.length; i += 2) {
-    let N = input[i].split(" ")[0];
-    let M = input[i].split(" ")[1];
-    let documentImportance = input[i + 1];
-    let documentImportanceIndex = 
+    let N = input[i].split(" ")[0]; // 문서의 개수
+    let M = input[i].split(" ")[1]; // 몇 번째로 인쇄되었는지 궁금한 문서의 순서
+    let documentImportance = input[i + 1].split("").map((i) => Number(i));
+    // 문서 중요도 인덱스 생성
+    let documentImportanceIndex = [];
+    for (let j = 0; j <= N; j++) {
+      documentImportanceIndex.push(j);
+    }
+    let tempDocumentImpotance = [];
+    let tempDocumentImpotanceIndex = [];
+    let printOrder = 0;
+    while (true) {
+      if (documentImportance[0] != Math.max(documentImportance)) {
+        tempDocumentImpotance.push(documentImportance.shift());
+        documentImportance.push(tempDocumentImpotance.shift());
+        tempDocumentImpotanceIndex.push(documentImportanceIndex.shift());
+        documentImportanceIndex.push(tempDocumentImpotanceIndex.shift());
+      } else {
+        printOrder++;
+        if (documentImportanceIndex[0] == M) {
+          break;
+        } else {
+          documentImportance.shift();
+          documentImportanceIndex.shift();
+        }
+      }
+    }
+    console.log(printOrder);
   }
 }
