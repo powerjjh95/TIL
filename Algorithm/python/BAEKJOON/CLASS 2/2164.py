19cf3f8d1d4ae5aac2df182c2e4f821c3b8d65d8
powerjjh95 <powerjjh95@gmail.com>
2024-03-21 03:14:40 +0900

    20240321

---
+++ Algorithm/python/BAEKJOON/CLASS 2/2164.py
@@ -0,0 +1,15 @@
+# TRY 2
+
+# # TRY 1
+# N = int(input())
+# cards = [i for i in range(1, N+1)]
+# while True:
+#     if len(cards) > 1:
+#         cards.pop(0)
+#         temporary_card = cards.pop(0)
+#         cards.append(temporary_card)
+#     else:
+#         break
+# print(cards[0])
+# # 시간 초과 발생
+# # deque 개념 사용
