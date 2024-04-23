fab4d8ea0eb738194e9910c623496e1f6167a225
powerjjh95 <powerjjh95@gmail.com>
2024-04-23 15:07:24 +0900

    20240423

--- Algorithm/javascript/BAEKJOON/CLASS 2/2164.js
+++ Algorithm/javascript/BAEKJOON/CLASS 2/2164.js
@@ -1,20 +1,103 @@
-//  TRY1
+// TRY 2
+/*
+- LinkedList 사용
+- 참고
+  - https://sj0826.github.io/structure/structure-%EC%97%B0%EA%B2%B0%EB%A6%AC%EC%8A%A4%ED%8A%B8/
+- 추후 공부 방향
+  - class 사용하는 방법 배우기
+*/
+
+// 데이터 입출력
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-input = [];
 rl.on("line", (line) => {
-  if (!line) {
-    rl.close();
-  } else {
-    input.push(line);
-  }
+  input = line;
+  rl.close();
 }).on("close", () => {
   solution(input);
   process.exit();
 });
 
-function solution() {}
+// LinkedList
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  add(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this.size++;
+
+    return newNode;
+  }
+  getHead() {
+    return this.head.value;
+  }
+  removeHead() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this.size--;
+  }
+  getSize() {
+    return this.size;
+  }
+}
+
+function solution(input) {
+  let N = Number(input);
+  let cards = new LinkedList();
+  for (let i = 1; i <= N; i++) {
+    cards.add(i);
+  }
+
+  while (cards.getSize() != 1) {
+    cards.removeHead();
+    cards.add(cards.getHead());
+    cards.removeHead();
+  }
+
+  // console.log(cards);
+  console.log(cards.getHead());
+}
+
+// // TRY 1
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// input = [];
+// rl.on("line", (line) => {
+//   if (!line) {
+//     rl.close();
+//   } else {
+//     input.push(line);
+//   }
+// }).on("close", () => {
+//   solution(input);
+//   process.exit();
+// });
+
+// function solution() {}
--- Algorithm/python/BAEKJOON/CLASS 2/2164.py
+++ Algorithm/python/BAEKJOON/CLASS 2/2164.py
@@ -10,20 +10,21 @@ while len(cards) > 1:
     cards.append(cards.popleft())
 print(cards[0])
 
-# REVIEW
-# # DEQUE(Double-Ended-QUEue)
-# # # 양방향에서 데이터를 처리할 수 있는 자료구조
-# # # deque.append(x) : x를 데크의 오른쪽 끝에 삽입한다
-# # # deque.appendleft(x) : x를 데크의 왼쪽 끝에 삽입한다
-# # # deque.pop() : 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서는 삭제한다
-# # # deque.popleft() : 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서는 삭제한다
-# # # deque.extend(array) : 배열 array를 순환하면서 데크의 오른쪽에 추가한다
-# # # deque.extendleft(array) : 배열 array를 순환하면서 데크의 왼쪽에 추가한다
-# # # deque.remove(x) : x를 데크에서 찾아 삭제한다
-# # # deque.rotate(num) : 데크를 num만큼 회전한다 (양수면 오른쪽, 음수는 왼쪽으로)
-# # # List보다 속도가 빠르고 쓰레드 환경에서 안전
-# # # pop(0)는 O(N) 연산을 수행하지만 deque는 O(1) 연산을 수행
-
+"""
+- REVIEW
+    - DEQUE(Double-Ended-QUEue)
+    - 양방향에서 데이터를 처리할 수 있는 자료구조
+    - deque.append(x) : x를 데크의 오른쪽 끝에 삽입한다
+    - deque.appendleft(x) : x를 데크의 왼쪽 끝에 삽입한다
+    - deque.pop() : 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서는 삭제한다
+    - deque.popleft() : 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서는 삭제한다
+    - deque.extend(array) : 배열 array를 순환하면서 데크의 오른쪽에 추가한다
+    - deque.extendleft(array) : 배열 array를 순환하면서 데크의 왼쪽에 추가한다
+    - deque.remove(x) : x를 데크에서 찾아 삭제한다
+    - deque.rotate(num) : 데크를 num만큼 회전한다 (양수면 오른쪽, 음수는 왼쪽으로)
+    - List보다 속도가 빠르고 쓰레드 환경에서 안전
+    - pop(0)는 O(N) 연산을 수행하지만 deque는 O(1) 연산을 수행
+"""
 
 # # TRY 1
 # N = int(input())
@@ -37,6 +38,10 @@ print(cards[0])
 #         break
 # print(cards[0])
 #
-# # REVIEW
-# # # 시간 초과 발생
-# # # deque 개념 사용
+
+
+"""
+- REVIEW
+    - 시간 초과 발생
+    - deque 개념 사용
+"""
