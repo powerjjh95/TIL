af3bf1442576a6bde23e171199175e0669045811
powerjjh95 <powerjjh95@gmail.com>
2024-06-05 15:02:43 +0900

    20240605 2775.js

--- Algorithm/javascript/BAEKJOON/CLASS 2/2775.js
+++ Algorithm/javascript/BAEKJOON/CLASS 2/2775.js
@@ -1,6 +1,6 @@
 // TRY 1
 /*
-- Javascript에서 2차원 리스트로 표현하는 것이 어려움
+- Javascript에서 2차원 배열로 표현하는 것이 어려움
 */
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -22,17 +22,25 @@ rl.on("line", (line) => {
 
 function solution(input) {
   let T = Number(input[0]);
-  console.log();
   for (let i = 1; i <= T; i++) {
     let [k, n] = [Number(input[2 * i - 1]), Number(input[2 * i])]; // k : 층, n : 호
-    let apartment = new Array(k + 1);
-
+    // 2차원 배열
+    let apartment = new Array(k + 1); // apartment의 배열의 개수를 k+1 개 만큼 설정
     // c : 층, r : 호
-    // for (let c = 0; c <= k; c++) {
-    for (let r = 0; r < n; r++) {
-      apartment[r] = new Array(k + 1);
+    for (let c = 0; c <= k; c++) {
+      // 배열의 개수만큼 순회하며 새로운 n개의 배열 생성
+      apartment[c] = new Array(n);
     }
-    // }
-    console.log(apartment[0][0]);
+    for (let c = 1; c <= k; c++) {
+      for (let r = 0; r < n; r++) {
+        apartment[0][r] = r + 1; // 0층의 각 r호는 r만큼 배당
+        apartment[c][r] = 0; // 1층 이상의 각 호는 0으로 설정
+        for (let i = 0; i <= r; i++) {
+          apartment[c][r] += apartment[c - 1][i]; // 각 층의 각 호수별 인원을 설정
+        }
+      }
+    }
+    let answer = apartment[k][n - 1];
+    console.log(answer);
   }
 }
