3cd2c7b94d26ffcf14f53ea409266372bdb1d2b2
powerjjh95 <powerjjh95@gmail.com>
2024-06-18 10:53:44 +0900

    certification 정보처리기사

---
+++ Algorithm/python/BAEKJOON/CLASS 2/4949.py
@@ -0,0 +1,12 @@
+while True:
+    string = input()
+    string_list = []
+
+    for character in string:
+        string_list.append(character)
+        if character == "(" or "[":
+
+    if string == "":
+        break
+    print(string_list)
+
